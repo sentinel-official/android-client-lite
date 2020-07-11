b7415a7dffd78fccd67912e4d31a414e101ae30a
cyberwolfdevf <cyberwolfdev98@gmail.com>
2020-07-11 18:48:39 +0530

    removed bulk of redundant code and resources

---
+++ app/src/main/java/co/sentinel/lite/util/FlavourHelper.java
@@ -0,0 +1,15 @@
+package co.sentinel.lite.util;
+
+import co.sentinel.lite.BuildConfig;
+
+public class FlavourHelper {
+
+    private static final String FLAVOUR_LANG_ALL = "liveBuild";
+
+    private static final String CODE_LANG_ALL = "en";
+
+    public static String getDefaultLanguageCode() {
+
+        return CODE_LANG_ALL;
+    }
+}
