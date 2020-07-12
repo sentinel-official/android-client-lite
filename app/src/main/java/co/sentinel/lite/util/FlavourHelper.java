f5c9401f01a6e40868b58f354654230a06b54ddb
cyberwolfdevf <cyberwolfdev98@gmail.com>
2020-07-12 20:30:16 +0530

    finalized welcome messages

--- app/src/main/java/co/sentinel/lite/util/FlavourHelper.java
+++ app/src/main/java/co/sentinel/lite/util/FlavourHelper.java
@@ -1,10 +1,6 @@
 package co.sentinel.lite.util;
 
-import co.sentinel.lite.BuildConfig;
-
 public class FlavourHelper {
-
-    private static final String FLAVOUR_LANG_ALL = "liveBuild";
 
     private static final String CODE_LANG_ALL = "en";
 
